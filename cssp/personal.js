f7363d8146d9c07d89f536c07746f9a7970b6d22
Rohan Chaudhary <rohanchaudhary.bkbiet2022@gmail.com>
2023-02-12 22:28:49 +0530

    Yahh boy

--- cssp/personal.js
+++
@@ -1,9 +0,0 @@
-$(".animated-progress span").each(function () {
-    $(this).animate(
-      {
-        width: $(this).attr("data-progress") + "%",
-      },
-      1000
-    );
-    $(this).text($(this).attr("data-progress") + "%");
-  });
